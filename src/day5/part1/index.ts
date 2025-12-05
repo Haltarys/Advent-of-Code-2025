22d8eef533e43f0eaaa7cf8e6590cdf49fe5c1ca
Haltarys <45515869+Haltarys@users.noreply.github.com>
2025-12-05 16:40:30 +0100

    feat(day5/part1): solve AoC 2025 day 5 part 1

---
+++ src/day5/part1/index.ts
@@ -0,0 +1,66 @@
+import { createReadStream, type PathLike } from 'fs';
+import { join } from 'path';
+import readline from 'readline/promises';
+
+type IngredientRange = {
+  lowerBound: number;
+  higherBound: number;
+};
+
+/**
+ * Solution to [Advent of Code 2025 Day 5 Part 1](https://adventofcode.com/2025/day/5)
+ *
+ * @param filePath path to the file containing the fresh ingredient ranges and the list of ingredients to test
+ * @returns The number of fresh ingredients IDs
+ */
+export async function day5(filePath: PathLike): Promise<number> {
+  const rl = readline.createInterface({ input: createReadStream(filePath) });
+  let freshIngredientIdsCount = 0;
+
+  const freshIngredientIdRanges: IngredientRange[] = [];
+
+  // Read fresh ingredient id ranges until we hit an empty line
+  for await (const line of rl) {
+    if (line === '') break;
+    const match = line.match(/(?<lower>\d+)-(?<higher>\d+)/);
+
+    // TODO: One could to merge and extend the ranges as they are added to avoid duplication and unnecessary checks
+
+    // Add each fresh ingredient range to a list
+    freshIngredientIdRanges.push({
+      lowerBound: parseInt(match!.groups!.lower),
+      higherBound: parseInt(match!.groups!.higher),
+    });
+  }
+
+  console.debug(freshIngredientIdRanges);
+
+  // Read each ingredient id to check if it falls within some range of fresh ingredient IDs
+  for await (const line of rl) {
+    const ingredientId = parseInt(line);
+
+    // If so, keep track of the total number of fresh ingredient IDs
+    if (
+      freshIngredientIdRanges.some(
+        ({ lowerBound, higherBound }) => ingredientId >= lowerBound && ingredientId <= higherBound
+      )
+    )
+      freshIngredientIdsCount++;
+
+    console.debug(ingredientId);
+  }
+
+  console.debug(`Number of fresh ingredients: ${freshIngredientIdsCount}`);
+
+  return freshIngredientIdsCount;
+}
+
+function main() {
+  const filePath = join(process.cwd(), 'assets/day5/input.txt');
+
+  day5(filePath);
+}
+
+main();
+
+export default day5;
