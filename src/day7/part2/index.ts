e9c0a5aeef3ef60813c39a734b444cc89173d2e5
Haltarys <45515869+Haltarys@users.noreply.github.com>
2025-12-07 19:56:55 +0100

    feat(day7/part2): solve AoC 2025 day 7 part 2

---
+++ src/day7/part2/index.ts
@@ -0,0 +1,74 @@
+import { createReadStream, type PathLike } from 'fs';
+import { join } from 'path';
+import readline from 'readline/promises';
+
+function computeTimelinesCount(
+  rows: string[],
+  rowIndex: number,
+  beamIndex: number,
+  memo: Map<string, number>
+): number {
+  // Base case: reached the bottom of the diagram
+  if (rowIndex >= rows.length) return 1;
+
+  // Create a unique key for this state
+  const key = `${rowIndex}-${beamIndex}`;
+
+  // Return cached result if available
+  if (memo.has(key)) {
+    return memo.get(key)!;
+  }
+
+  const row = rows[rowIndex];
+
+  let res: number;
+  // If we have reached a splitter, compute the number of paths to its left and to its right
+  if (row[beamIndex] === '^') {
+    const leftPathTimelineCount = computeTimelinesCount(rows, rowIndex + 1, beamIndex - 1, memo);
+    const rightPathTimelineCount = computeTimelinesCount(rows, rowIndex + 1, beamIndex + 1, memo);
+
+    res = leftPathTimelineCount + rightPathTimelineCount;
+  } else {
+    // Continue straight down
+    res = computeTimelinesCount(rows, rowIndex + 1, beamIndex, memo);
+  }
+
+  // Store result in memo before returning
+  memo.set(key, res);
+  return res;
+}
+
+/**
+ * Solution to [Advent of Code 2025 Day 7 Part 2](https://adventofcode.com/2025/day/7#part2)
+ *
+ * @param filePath path to the tachyon manifold diagram
+ * @returns The number of different timelines a single tachyon will travel on
+ */
+export async function day7(filePath: PathLike): Promise<number> {
+  const rl = readline.createInterface({ input: createReadStream(filePath) });
+  const rows: string[] = [];
+
+  // Store the map in an array
+  for await (const line of rl) rows.push(line);
+
+  // Find the starting position index of the tachyon beam
+  const beamIndex = rows[0].indexOf('S')!;
+
+  // Map to memoise the result to improve performance
+  const memo = new Map<string, number>();
+  const timelineCount = computeTimelinesCount(rows, 0, beamIndex, memo);
+
+  console.debug(`A single tachyon will travel on ${timelineCount} different timelines.`);
+
+  return timelineCount;
+}
+
+function main() {
+  const filePath = join(process.cwd(), 'assets/day7/input.txt');
+
+  day7(filePath);
+}
+
+main();
+
+export default day7;
